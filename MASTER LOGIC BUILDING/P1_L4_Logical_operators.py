f40293c5c12edf27ed9aacd94848a71d45164b3d
Komal <komalhiremath713@gmail.com>
2026-02-11 18:02:46 +0530

    Day 5

---
+++ MASTER LOGIC BUILDING/P1_L4_Logical_operators.py
@@ -0,0 +1,29 @@
+# 1. Take a character and check if it is a letter, a digit, or neither. 
+s = input("Enter the char: ")
+if "a" <= s <= "z" or "A" <= s <= "Z":
+    print("Its a letter")
+elif "0" <= s <= "9":
+    print("Its a digit")
+else:
+    print("neither")
+    
+# 2. Take a number and print “Fizz” if divisible by 3, “Buzz” if divisible by 5, and “FizzBuzz” if divisible by both. 
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer = []
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                answer.append("FizzBuzz") 
+            elif i % 3 == 0:
+                answer.append("Fizz")
+            elif i % 5 == 0 :
+                answer.append("Buzz") 
+            else:
+                answer.append(str(i))
+        return answer
+  # “Return an array/list of size N” 
+#✅ Create empty list
+# ✅ Loop
+# ✅ Append inside loop
+# ❌ Never return inside loop     
+ 
