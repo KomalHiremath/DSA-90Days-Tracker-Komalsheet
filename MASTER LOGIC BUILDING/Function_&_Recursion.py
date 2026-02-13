904125c8166eca7893dbcc08e72e5a57a3228004
Komal <komalhiremath713@gmail.com>
2026-02-13 21:36:22 +0530

    Day 7 continued

---
+++ MASTER LOGIC BUILDING/Function_&_Recursion.py
@@ -0,0 +1,9 @@
+### FUNCTION ####################################################################################################
+# AVerage of 3 numbers using function
+
+def cal_avg(a, b, c):
+    avg =(a+b+c)/3
+    print(avg)
+cal_avg(111, 278, 3567)
+
+    
