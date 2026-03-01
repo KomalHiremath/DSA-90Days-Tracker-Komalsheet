1a7de36af4b3f8c8f09297d25ef8c1ac7c6a9580
Komal Hiremath <126691366+KomalHiremath@users.noreply.github.com>
2026-03-01 23:19:27 +0530

    Update IBM_CodingQue.py

--- MASTER LOGIC BUILDING/IBM_CodingQue.py
+++ MASTER LOGIC BUILDING/IBM_CodingQue.py
@@ -14,161 +14,161 @@ class Solution:
             r -=1
         return "".join(S)
     
-# ------ 1. PRIME CHECK (OPTIMIZED √N VERSION)  -----
-# ------ 1. PRINT PRIMES (VARIATION OF PRIME LOGIC) ----
+------ 1. PRIME CHECK (OPTIMIZED √N VERSION)  -----
+------ 1. PRINT PRIMES (VARIATION OF PRIME LOGIC) ----
 
-# def prime(num):
-#     if num<=1:
-#         return False
+def prime(num):
+    if num<=1:
+        return False
     
-#     for i in range(2, int(num**0.5)+1):
-#         if num%i == 0:
-#             return False
-#     return True
+    for i in range(2, int(num**0.5)+1):
+        if num%i == 0:
+            return False
+    return True
     
-# # num = int(input("Enter the num: ")) 
-# # print(prime(num))  
+# num = int(input("Enter the num: ")) 
+# print(prime(num))  
 
-# # ---- 2. COUNT DIGITS ----------
+# ---- 2. COUNT DIGITS ----------
 
-# def count(n):
-#     return(len(str(abs(n))))
+def count(n):
+    return(len(str(abs(n))))
 
-# #     n = abs(n) 
-# #     if n == 0:
-# #         return 1
-# #     count = 0
-# #     while n > 0:
-# #         count +=1
-# #         n = n//10
-# #     return count
-
-# n = int(input("Enter the num: ")) 
-# print(count(n))
-
-# # --- 2. COUNT THE NUMBER OF DIGITS IN N THAT DIVIDE N EVENLY ------
-
-# # Input: n = 2446 Output: 1
-# # Explanation: Here among 2, 4, 6 only 2 divides 2446 evenly while 4 and 6 do not.
-
-# # Input: n = 23  Output: 0
-# # Explanation: 2 and 3, none of them divide 23 evenly.
-
-# # Input: n = 12 Output: 2
-# # Explanation: 1, 2 when both divide 12 leaves remainder 0.
-
-
-# def evenlyDivides(self, n):
-#     original = abs(n)
+#     n = abs(n) 
+#     if n == 0:
+#         return 1
 #     count = 0
-    
 #     while n > 0:
-#         digit = n % 10
-        
-#         if digit != 0 and original % digit == 0:
-#             count += 1
-        
-#         n //= 10
-    
+#         count +=1
+#         n = n//10
 #     return count
 
-# # --- 1. REVERSE NUMBER ----
-# def reverseDigits(self, n):
-#     rev = 0
-       
-#     while n > 0:
-#         digit = n % 10
-#         rev = rev * 10 + digit
-#         n = n // 10
+n = int(input("Enter the num: ")) 
+print(count(n))
 
-# #     return rev
+# --- 2. COUNT THE NUMBER OF DIGITS IN N THAT DIVIDE N EVENLY ------
 
-# # --- 2. PALINDROME NUMBER ----
-# def pal(n):
-#     rev = 0
-#     original = n
+# Input: n = 2446 Output: 1
+# Explanation: Here among 2, 4, 6 only 2 divides 2446 evenly while 4 and 6 do not.
+
+# Input: n = 23  Output: 0
+# Explanation: 2 and 3, none of them divide 23 evenly.
+
+# Input: n = 12 Output: 2
+# Explanation: 1, 2 when both divide 12 leaves remainder 0.
+
+
+def evenlyDivides(self, n):
+    original = abs(n)
+    count = 0
     
-#     while n > 0:
-#         digit = n % 10
-#         rev = rev*10 + digit
-#         n = n//10
-#     return rev == original
-# n = int(input("enter the num: "))
-# if pal(n):
-#     print("true")
-# else:
-#     print("False")
+    while n > 0:
+        digit = n % 10
         
-#  # --- 2. PALINDROME STRING ----       
-# def pal(s):
-#     return(s[::-1]) == s
-# s =input("ENter the string: ")
-# print(pal(s))
-
-# # --- 3. SUM OF DIGITS ----
-# def num(n):
-#     if n == 0:
-#         return 0
-#     last = n % 10
-#     rem = n // 10
-#     return last + num(rem)
+        if digit != 0 and original % digit == 0:
+            count += 1
+        
+        n //= 10
     
-# n = int(input("NUm : "))
-# print(num(n))
+    return count
 
-# # --- 4. GCD (EUCLIDEAN METHOD) ----
-# def gcd(a,b):
-#     while b!= 0:
-#         a, b = b , a%b
-#     return a
-# a = int(input("NUm : "))
-# b = int(input("NUm : "))
-# print(gcd(a, b))
+# --- 1. REVERSE NUMBER ----
+def reverseDigits(self, n):
+    rev = 0
+       
+    while n > 0:
+        digit = n % 10
+        rev = rev * 10 + digit
+        n = n // 10
 
-###### OR ###########
+#     return rev
 
-# import math
-# a = int(input("Num : "))
-# b = int(input("NUm : "))
-# print(math.gcd(a,b))
+# --- 2. PALINDROME NUMBER ----
+def pal(n):
+    rev = 0
+    original = n
+    
+    while n > 0:
+        digit = n % 10
+        rev = rev*10 + digit
+        n = n//10
+    return rev == original
+n = int(input("enter the num: "))
+if pal(n):
+    print("true")
+else:
+    print("False")
+        
+ # --- 2. PALINDROME STRING ----       
+def pal(s):
+    return(s[::-1]) == s
+s =input("ENter the string: ")
+print(pal(s))
 
-#  # --- 5. LCM ----
-# def gcd(a,b):
-#     while b!= 0:
-#         a, b = b , a%b
-#     return a
-# def lcm(a,b):
-#     return (a*b) // gcd(a,b)
-# a = int(input("Num : "))
-# b = int(input("NUm : "))
-# print(lcm(a,b))
+# --- 3. SUM OF DIGITS ----
+def num(n):
+    if n == 0:
+        return 0
+    last = n % 10
+    rem = n // 10
+    return last + num(rem)
+    
+n = int(input("NUm : "))
+print(num(n))
+
+# --- 4. GCD (EUCLIDEAN METHOD) ----
+def gcd(a,b):
+    while b!= 0:
+        a, b = b , a%b
+    return a
+a = int(input("NUm : "))
+b = int(input("NUm : "))
+print(gcd(a, b))
+
+##### OR ###########
+
+import math
+a = int(input("Num : "))
+b = int(input("NUm : "))
+print(math.gcd(a,b))
+
+ # --- 5. LCM ----
+def gcd(a,b):
+    while b!= 0:
+        a, b = b , a%b
+    return a
+def lcm(a,b):
+    return (a*b) // gcd(a,b)
+a = int(input("Num : "))
+b = int(input("NUm : "))
+print(lcm(a,b))
  
-# # --- 6. FIBONACCI (LOOP VERSION) ---
+# --- 6. FIBONACCI (LOOP VERSION) ---
 
-# n = int(input("NUm : "))
-# a = 0
-# b = 1
-# c = 0
-# for i in range( n):
-#     print(c)
-#     a = b
-#     b =c
-#     c = a+b
+n = int(input("NUm : "))
+a = 0
+b = 1
+c = 0
+for i in range( n):
+    print(c)
+    a = b
+    b =c
+    c = a+b
 
-# # --- 7. SUM OF FIBONACCI (LOOP VERSION)  ----
-# n = int(input("Num: "))
-# a = 0 
-# b = 1
-# c = 0
-# total = 0
-# for i in range(n):
-#     print(c)
-#     total +=c
+# --- 7. SUM OF FIBONACCI (LOOP VERSION)  ----
+n = int(input("Num: "))
+a = 0 
+b = 1
+c = 0
+total = 0
+for i in range(n):
+    print(c)
+    total +=c
     
-#     a = b 
-#     b = c 
-#     c = a+b 
-# print("sum: ", total)
+    a = b 
+    b = c 
+    c = a+b 
+print("sum: ", total)
 
 # --- 6. FIBONACCI (Recursion)------------
 def fib(n):
