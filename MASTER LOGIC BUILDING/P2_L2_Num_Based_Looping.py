90a6f2a729ef16a9da376e2ba31ed5abf9bbb7cc
Komal <komalhiremath713@gmail.com>
2026-03-07 11:12:07 +0530

    Day 14

---
+++ MASTER LOGIC BUILDING/P2_L2_Num_Based_Looping.py
@@ -0,0 +1,23 @@
+# # 1. Count the number of digits in a given number. 
+# def countDigits(num):
+#     cnt = 0
+#     while num > 0:
+#         num = num//10
+#         cnt+=1
+#     return cnt
+# num = int(input("NUm : "))
+# print(countDigits(num))
+        
+# 2. Print the reverse of a given number. 
+def revNum(n):
+    return n[::-1]
+n = int(input("NUM: "))
+print(revNum(n))
+# 3. Check if a number is a palindrome. 
+# 4. Find the sum of digits of a number. 
+# 5. Check if a number is an Armstrong number. 
+# 6. Check if a number is a perfect number. 
+# 7. Print all prime numbers between 1 and 100. 
+# 8. Check if a number is prime or not. 
+# 9. Print Fibonacci series up to n terms. 
+# 10. Print sum of first n terms of Fibonacci series. 
