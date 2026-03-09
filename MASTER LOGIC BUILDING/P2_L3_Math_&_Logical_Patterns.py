b17c83553056152b0620cc9f9136914e14782b51
Komal <komalhiremath713@gmail.com>
2026-03-09 12:56:45 +0530

    Day 16

---
+++ MASTER LOGIC BUILDING/P2_L3_Math_&_Logical_Patterns.py
@@ -0,0 +1,29 @@
+# # # 1. Print the squares of numbers from 1 to n. 
+# # def squaresNum(n):
+# #     l = []
+# #     for i in range(1, n+1):
+# #         sq = i*i
+# #         l.append(sq)
+# #     return l
+# # n = int(input("NUM: "))
+# # print(squaresNum(n))
+    
+# # 2. Print cubes of numbers from 1 to n. 
+# def CubeNum(n):
+#     l = []
+#     for i in range(1, n+1):
+#         cube = i*i*i # i*3
+#         l.append(cube)
+#     return l
+# n = int(input("NUM: "))
+# print(CubeNum(n))
+
+# 3. Print all numbers between a and b divisible by 7. 
+
+# 4. Find HCF (GCD) of two numbers using loops. 
+# 5. Find LCM of two numbers using loops. 
+# 6. Print all factors of a given number. 
+# 7. Find the sum of all factors of a number. 
+# 8. Check if a number is a strong number (sum of factorials of digits = number). 
+# 9. Print first n terms of an arithmetic progression (a, d). 
+# 10. Print first n terms of a geometric progression (a, r). 
