314c4111bb276e2b30b915a6a219359904d00f22
Komal <komalhiremath713@gmail.com>
2026-03-17 12:45:13 +0530

    Day 21

--- MASTER LOGIC BUILDING/P4_L2_Searching_&_Counting_Logic.py
+++ MASTER LOGIC BUILDING/P4_L2_Searching_&_Counting_Logic.py
@@ -82,44 +82,101 @@
 #             index = i
 #     return index
 
+##### ---- OR ----- #####
+
 # def occ(arr):
 #     for i in range (len(arr)-1, -1, -1): # range(start, stop, step)
 #         if arr[i] == k:
 #             return i
-#     return 1
+#     return -1
+
 # n = int(input("Enter the size: "))
 # arr = element(n)
 # k = int(input("Enter the num to search: "))
 # print(occ(arr))
 
 
-# 5. Find the first and last occurrence of a given number. 
+# # 5. Find the first and last occurrence of a given number. 
+# def element(n):
+#     arr =[]
+#     for i in range(n):
+#         arr.append(int(input()))
+#     return arr
+
+# def first_occ(arr, k):
+#     for i in range (len(arr)):
+#         if arr[i] == k:
+#             return i
+#     return -1
+
+# def last_occ(arr, k):
+#     for i in range (len(arr)-1, -1, -1): # range(start, stop, step)
+#         if arr[i] == k:
+#             return i
+#     return -1
+
+# n = int(input("Enter the size: "))
+# arr = element(n)
+# k = int(input("Enter the num to search: "))
+# print(first_occ(arr, k), last_occ(arr, k))
+
+
+# # 6. Find the sum of even elements only. 
+
+# def element(n):
+#     arr =[]
+#     for i in range(n):
+#         arr.append(int(input()))
+#     return arr
+
+# def even(arr):
+#     total = 0
+#     for i in arr:
+#         if i%2 == 0:
+#             total += i
+#     return total
+        
+# n = int(input("Enter the size: "))
+# arr = element(n)
+# print("The sum is:", even(arr))
+
+# # 7. Find the sum of odd elements only. 
+# def element(n):
+#     arr =[]
+#     for i in range(n):
+#         arr.append(int(input()))
+#     return arr
+
+# def odd(arr):
+#     total = 0
+#     for i in arr:
+#         if i%2 != 0:
+#             total += i
+#     return total
+        
+# n = int(input("Enter the size: "))
+# arr = element(n)
+# print("The sum is:", odd(arr))
+
+# 8. Check if all elements in an array are unique. 
 def element(n):
     arr =[]
     for i in range(n):
         arr.append(int(input()))
     return arr
-
-def first_occ(arr, k):
-    for i in range (len(arr)):
-        if arr[i] == k:
-            return i
-    return -1
-
-def last_occ(arr, k):
-    for i in range (len(arr)-1, -1, -1): # range(start, stop, step)
-        if arr[i] == k:
-            return i
-    return -1
-
+def unique(arr):
+    j = arr[i] +1
+    for i in range(len(arr)+1):
+        if j==i:
+            return False
+    return True
 n = int(input("Enter the size: "))
 arr = element(n)
-k = int(input("Enter the num to search: "))
-print(first_occ(arr, k), last_occ(arr, k))
+print(unique(arr))
 
-# 5. Check if all elements in an array are unique. 
-# 6. Find the sum of even elements only. 
-# 7. Find the sum of odd elements only. 
+        
+
+
 # 8. Find the count of prime numbers in the array. 
 # 9. Count how many numbers are divisible by 3 and 5 both. 
 # 10. Count how many elements are perfect squares. 
