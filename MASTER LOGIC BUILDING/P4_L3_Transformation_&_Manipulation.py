08803db6954edddc5b91c9b229613aecf3f5ed38
Komal <komalhiremath713@gmail.com>
2026-05-21 14:56:46 +0530

    DSA rotation problem

--- MASTER LOGIC BUILDING/P4_L3_Transformation_&_Manipulation.py
+++ MASTER LOGIC BUILDING/P4_L3_Transformation_&_Manipulation.py
@@ -192,6 +192,24 @@
     
     
 # 8. Rotate an array by one position to the right. 
+def elem(n):
+    arr = []
+    for i in range(n):
+        arr.append(int(input()))
+    return arr
+
+def rotateR(arr):
+    n = len(arr)
+    temp = arr[n-1]
+    for i in range(n-1, 0, -1):
+        arr[i] = arr[i-1]
+    arr[0] = temp
+    return arr
+
+n = int(input("Enter the len of arr: "))
+arr = elem(n)
+print(rotateR(arr))
+        
 # 9. Swap alternate elements (1st ↔ 2nd, 3rd ↔ 4th, etc.). 
 # 10. Copy one array to another manually. 
 
