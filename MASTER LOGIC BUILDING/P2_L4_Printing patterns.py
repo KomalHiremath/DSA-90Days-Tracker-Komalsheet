2124d16e81955933ff79a9d0820c046162965c76
Komal <komalhiremath713@gmail.com>
2026-06-01 14:19:15 +0530

    solved 4 problems

--- MASTER LOGIC BUILDING/P2_L4_Printing patterns.py
+++ MASTER LOGIC BUILDING/P2_L4_Printing patterns.py
@@ -29,10 +29,46 @@ n = int(input("Enter the num of stars:"))
 # for i in range(1, n+1):
 #     print("*" * i)
 
-# 6. Print a Right-Aligned Triangle of Stars
-for i in range(1, n+1):
-    print(" " * (n-i) + "*" * i)
+# # 6. Print a Right-Aligned Triangle of Stars
+# #   *
+# #  **
+# # ***
+# for i in range(1, n+1):
+#     print(" " * (n-i) + "*" * i)
 
+
+# # 7. Print Stars in Even Numbers (2, 4, 6, 8, 10)
+# # **
+# # ****
+# # ******
+# # ********
+# for i in range(1, n+1):
+#     print("*" * (i+i))
+
+# 8. Print Stars in Odd Numbers (1, 3, 5, 7, 9)
+# $
+# $$$
+# $$$$$
+# $$$$$$$
+# $$$$$$$$$
+# for i in range(1, n+1):
+#     print("$" * (i+(i-1)))
+
+# 9. Print a Centered Pyramid of Stars
+#    &   
+#   &&&  
+#  &&&&& 
+# &&&&&&&
+# for i in range(1, n+1):
+#     print(" " * (n-i) + "&" * (i+(i-1)) + " " * (n-i))
+
+# 10. Print Stars and Spaces Alternating (Stars and Blank Spaces)
+# ---*
+# --* *
+# -* * *
+# * * * *
+# for i in range(1, n+1):
+#     print("-" * (n-i) + "* " * (i-1) + "*")
 
 
 
